d82538f5440d562965284adcb605477a26cdc747
Pingvu <nhlpool27@gmail.com>
2023-01-18 10:01:05 +0100

    Add project files.

---
+++ uppgift 1.2 cs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace uppgift1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Du är nästan klar med kapitel 1" + "\n Du har lärt dig skriva ut tecken som \" och \\");
+        }
+    }
+}
